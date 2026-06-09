a9f271894d50c0464a1c83818fe57042f33f3d08
snideepmondal <57210671+snideepmondal@users.noreply.github.com>
2026-06-09 10:39:55 +0530

    Create CartSlice.jsx

---
+++ CartSlice.jsx
@@ -0,0 +1,37 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  cartItems: [],
+};
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {
+    addItem: (state, action) => {
+      const item = state.cartItems.find(i => i.id === action.payload.id);
+      if (item) {
+        item.quantity += 1;
+      } else {
+        state.cartItems.push({ ...action.payload, quantity: 1 });
+      }
+    },
+
+    removeItem: (state, action) => {
+      state.cartItems = state.cartItems.filter(
+        item => item.id !== action.payload
+      );
+    },
+
+    updateQuantity: (state, action) => {
+      const { id, quantity } = action.payload;
+      const item = state.cartItems.find(i => i.id === id);
+      if (item && quantity > 0) {
+        item.quantity = quantity;
+      }
+    },
+  },
+});
+
+export const { addItem, removeItem, updateQuantity } = cartSlice.actions;
+export default cartSlice.reducer;
