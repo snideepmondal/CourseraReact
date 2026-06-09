73ffe487b0f758846ae22dff45cc4a0673db12ce
snideepmondal <57210671+snideepmondal@users.noreply.github.com>
2026-06-09 10:38:40 +0530

    Create AboutUs.jsx

---
+++ AboutUs.jsx
@@ -0,0 +1,27 @@
+
+---
+
+# 📁 2. `AboutUs.jsx`
+
+```jsx
+import React from "react";
+
+const AboutUs = () => {
+  return (
+    <div className="about-container">
+      <h1>About Paradise Nursery</h1>
+      <p>
+        Paradise Nursery is dedicated to bringing nature closer to your home.
+        We offer a wide variety of indoor and outdoor plants to make your
+        surroundings greener and healthier.
+      </p>
+
+      <p>
+        Our mission is to promote sustainable living by encouraging people to
+        grow and care for plants.
+      </p>
+    </div>
+  );
+};
+
+export default AboutUs;
